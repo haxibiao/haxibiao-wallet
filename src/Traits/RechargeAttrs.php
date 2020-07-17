c439979b88efd274c20e9c09b3728d0079be8239
曾达威 <zengdawei@haxibiao.com>
2020-07-17 10:18:33 +0800

    完善充值逻辑，只要我们没有收到钱，就是充值失败

--- src/Traits/RechargeAttrs.php
+++ src/Traits/RechargeAttrs.php
@@ -8,7 +8,11 @@ trait RechargeAttrs
 {
     public function getStatusMsgAttribute()
     {
-        return Recharge::getPayStatuses()[$this->status];
+        // 只要我们没有收到钱，就是充值失败
+        if ($this->status != Recharge::RECHARGE_SUCCESS) {
+            return '充值失败';
+        }
+        return '充值成功';
     }
 
     public function getPlatformMsgAttribute()
