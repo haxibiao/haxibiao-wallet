3d5e02e5a09fc1335ffb503b6de48609db6daea4
曾达威 <zengdawei@haxibiao.com>
2020-08-04 15:48:59 +0800

    update aliapy config

--- config/pay.php
+++ config/pay.php
@@ -2,22 +2,24 @@
 
 return [
     'alipay' => [
-        'app_id'         => env('ALIPAY_PAY_APPID'),
-        'notify_url'     => env('APP_URL') . '/api/pay/alipay-notify',
-        'return_url'     => env('APP_URL') . '/api/pay/alipay-return',
-        'private_key'    => file_get_contents(base_path('cert/alipay/private_key')),
-        'ali_public_key' => file_get_contents(base_path('cert/alipay/public_key')),
-        'log'            => [
+        'app_id'              => env('ALIPAY_PAY_APPID'),
+        'notify_url'          => env('APP_URL') . '/api/pay/alipay-notify',
+        'return_url'          => env('APP_URL') . '/api/pay/alipay-return',
+        'private_key'         => file_get_contents(base_path('cert/alipay/private_key')),
+        'ali_public_key'      => base_path('cert/alipay/alipayCertPublicKey_RSA2.crt'),
+        'app_cert_public_key' => base_path('cert/alipay/appCertPublicKey_2021001172621778.crt'), //应用公钥证书路径
+        'alipay_root_cert'    => base_path('cert/alipay/alipayRootCert.crt'),
+        'log'                 => [
             'file'     => storage_path('logs/pay/alipay.log'),
             'level'    => 'info',
             'type'     => 'daily',
             'max_file' => 30,
         ],
-        'http'           => [
+        'http'                => [
             'timeout'         => 30,
             'connect_timeout' => 30,
         ],
-        'mode'           => 'normal',
+        'mode'                => 'normal',
     ],
     'wechat' => [
         'appid'       => env('WECHAT_APPID'),
