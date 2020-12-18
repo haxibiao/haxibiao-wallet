b80b18592aca13673422efef577762a737ee1e7c
ivan <ivan@haxibiao.com>
2020-12-18 19:59:13 +0800

    提现需要提前支付宝部署自己证书到服务器

--- config/pay.php
+++ config/pay.php
@@ -5,10 +5,10 @@ return [
         'app_id'              => env('ALIPAY_PAY_APPID'),
         'notify_url'          => env('APP_URL') . '/api/pay/alipay-notify',
         'return_url'          => env('APP_URL') . '/api/pay/alipay-return',
-        'private_key'         => file_get_contents(base_path('cert/alipay/private_key')),
-        'ali_public_key'      => base_path('cert/alipay/alipayCertPublicKey_RSA2.crt'),
-        'app_cert_public_key' => base_path('cert/alipay/appCertPublicKey_2021001172621778.crt'), //应用公钥证书路径
-        'alipay_root_cert'    => base_path('cert/alipay/alipayRootCert.crt'),
+        'private_key'         => file_get_contents('/etc/alipay/private_key'), //需要提前支付宝部署自己证书到服务器
+        'ali_public_key'      => '/etc/alipay/alipayCertPublicKey_RSA2.crt',
+        'app_cert_public_key' => '/etc/alipay/appCertPublicKey_2021001172621778.crt', //应用公钥证书路径
+        'alipay_root_cert'    => '/etc/alipay/alipayRootCert.crt',
         'log'                 => [
             'file'     => storage_path('logs/pay/alipay.log'),
             'level'    => 'info',
@@ -25,8 +25,8 @@ return [
         'appid'       => env('WECHAT_APPID'),
         'key'         => env('WECHAT_PAY_KEY'),
         'mch_id'      => env('WECHAT_PAY_MCH_ID'),
-        'cert_client' => base_path('cert/wechat/apiclient_cert.pem'),
-        'cert_key'    => base_path('cert/wechat/apiclient_key.pem'),
+        'cert_client' => '/etc/wechat/apiclient_cert.pem',
+        'cert_key'    => '/etc/wechat/apiclient_key.pem',
         'notify_url'  => env('APP_URL') . '/api/pay/wechat-notify',
         'log'         => [
             'file'     => storage_path('logs/pay/wechat.log'),
