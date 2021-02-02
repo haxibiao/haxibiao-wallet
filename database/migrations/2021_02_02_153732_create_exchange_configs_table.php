f75e189a881f68e0e808a46e227f186588346884
蔡志刚 <caizhigang@haxibiao.com>
2021-02-02 18:49:36 +0800

    enhanced migrations

---
+++ database/migrations/2021_02_02_153732_create_exchange_configs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExchangeConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_configs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('num')->index();
+            $table->string('name')->index();
+            $table->string('value');
+            $table->string('status')->comment('0:禁用，1启用');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_configs');
+    }
+}
