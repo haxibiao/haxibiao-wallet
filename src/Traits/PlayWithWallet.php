a40090a883172a4c1f4c599e5a6250668d36f940
唐淑娟 <tangshujuan@haxifang.cn>
2021-05-17 16:18:12 +0800

    add oauths relation

--- src/Traits/PlayWithWallet.php
+++ src/Traits/PlayWithWallet.php
@@ -2,6 +2,7 @@
 
 namespace Haxibiao\Wallet\Traits;
 
+use Haxibiao\Breeze\OAuth;
 use Haxibiao\Wallet\Recharge;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -10,5 +11,9 @@ trait PlayWithWallet
     public function recharges(): HasMany
     {
         return $this->hasMany(Recharge::class);
+    }
+    function oauths(): HasMany
+    {
+        return $this->hasMany(OAuth::class);
     }
 }
