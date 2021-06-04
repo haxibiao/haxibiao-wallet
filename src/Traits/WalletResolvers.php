bdcf2d371a9411ce86803ba8a77e794d507f5715
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-06-04 17:05:10 +0800

    设置支付宝绑定信息pay_account可以为空

--- src/Traits/WalletResolvers.php
+++ src/Traits/WalletResolvers.php
@@ -24,9 +24,10 @@ trait WalletResolvers
         if (!preg_match_all("/([\x{4e00}-\x{9fa5}]+)/u", $real_name)) {
             throw new GQLException('姓名输入不合法,请重新输入~');
         }
-
-        if ($args['pay_account'] ?? null && is_phone_number($args['pay_account']) == 0) {
-            throw new GQLException('目前只支持手机号绑定的支付宝账号哦,请重新输入 ~');
+        if (!$args['pay_account'] ?? null) {
+            if (is_phone_number($args['pay_account']) == 0) {
+                throw new GQLException('目前只支持手机号绑定的支付宝账号哦,请重新输入 ~');
+            }
         }
 
         $wallet              = Wallet::rmbWalletOf($user);
