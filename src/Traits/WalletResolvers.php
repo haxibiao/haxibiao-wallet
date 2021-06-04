761cd19113f9674899ab50cfe863da754cb52f53
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-06-04 17:46:01 +0800

    up..

--- src/Traits/WalletResolvers.php
+++ src/Traits/WalletResolvers.php
@@ -24,7 +24,7 @@ trait WalletResolvers
         if (!preg_match_all("/([\x{4e00}-\x{9fa5}]+)/u", $real_name)) {
             throw new GQLException('姓名输入不合法,请重新输入~');
         }
-        if (!$args['pay_account'] ?? null) {
+        if (!empty($args['pay_account'])) {
             if (is_phone_number($args['pay_account']) == 0) {
                 throw new GQLException('目前只支持手机号绑定的支付宝账号哦,请重新输入 ~');
             }
