ab7203718918156986f8eaffbb41acd231bc53d8
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-07-23 11:26:20 +0800

    up...

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,8 @@
 use Illuminate\Contracts\Routing\Registrar as RouteRegisterContract;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api'], function (RouteRegisterContract $api) {
-    // @ANY /api/live
-    $api->group(['prefix' => 'pay'], function (RouteRegisterContract $api) {
-        Route::any('/alipay-notify', PayController::class . '@alipayNotify');
-        Route::any('/alipay-return', PayController::class . '@alipayReturn');
-        Route::any('/wechat-notify', PayController::class . '@wechatNotify');
-    });
+Route::group(['prefix' => 'pay'], function (RouteRegisterContract $api) {
+    Route::any('/alipay-notify', PayController::class . '@alipayNotify');
+    Route::any('/alipay-return', PayController::class . '@alipayReturn');
+    Route::any('/wechat-notify', PayController::class . '@wechatNotify');
 });
